705a08a5cf992b602a7a809fcb7384f707cb93f0
mowtwo <1046099511@qq.com>
2022-03-22 16:58:37 +0800

    更新readme

--- src/App.svelte
+++ src/App.svelte
@@ -4,3 +4,14 @@
 </script>
 
 <Router {routes} />
+
+<style>
+  :global(html, body) {
+    margin: 0;
+    padding: 0;
+  }
+  :global(body) {
+    background-color: #fff;
+    font-family: 微软雅黑, "Microsoft YaHei", Helvetica, Tahoma, sans-serif;
+  }
+</style>
--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,10 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  base: './',
   server: {
     proxy: {
       '/api': 'http://127.0.0.1:8010'
     }
-  }
+  },
 })
